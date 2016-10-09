364721f5b7f23c4bdef80d9dddd9505188a07748
Jeferson IP Fidelis <Jeferson IP Fidelis@DESKTOP-VSNEN93>
2016-10-09 09:38:11 -0300

    Implementação do teclado de Membrana 4X4 com o LCD ili9341

--- LCDili9341Test/Program.cs
+++ LCDili9341Test/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using STM32f4NetMfLib;
 using STM32f4NetMfLib.LCDili9341.HelpersFonts;
 using System;
 using System.Threading;
@@ -9,6 +12,14 @@ namespace LCDili9341Test
     {
         static private STM32f4NetMfLib.LCDili9341.Driver tftLcd;
         private static bool flag;
+        private static Font font = new StandardFixedWidthFont();
+
+        static private char[] keyMap = new char[16]
+        {
+            '1','2','3','A','4','5','6','B','7','8','9','C','*','0','#','D'
+        };
+
+        static private string teclado;
 
         public static void Main()
         {
@@ -21,7 +32,7 @@ namespace LCDili9341Test
                                  dataCommandPin: Pins.PD6,
                                  backlightPin: Pins.PD7);
 
-            var font = new StandardFixedWidthFont();
+            
 
             tftLcd.ClearScreen();
             tftLcd.DrawString(10, 10, "LCD ILI9341 WITH STM32F4_DISCOVERY", 0xF800, font);
@@ -29,7 +40,23 @@ namespace LCDili9341Test
             tftLcd.DrawString(10, 30, "DATE AND TIME", 0x0000FF, font);
             tftLcd.BacklightOn = true;
 
-            System.Threading.Timer tmrClock = new System.Threading.Timer(new TimerCallback(OnClock), null, 5000, 1000);
+            /* CONFIG PINS FOR KEYPAD MATRIX 4x4 */
+            // Row pins. The keypad exists out of 4 rows.
+            // D0, D1, D2 e D3
+            Cpu.Pin[] RowPins = { Pins.PD0, Pins.PD1, Pins.PD2, Pins.PD3 };
+
+            // Col pins. The keypad exists out of 4 columns.
+            // C1, C2, C4 e C0
+            Cpu.Pin[] ColPins = { Pins.PC1, Pins.PC2, Pins.PC4, Pins.PC0 };
+
+            // Initializes the new keypad
+            KeyBoard kb = new KeyBoard(RowPins, ColPins);
+
+            // Bind both events
+            kb.OnKeyDown += new NativeEventHandler(kb_OnKeyDown);
+            kb.OnKeyUp += new NativeEventHandler(kb_OnKeyUp);
+
+            System.Threading.Timer tmrClock = new System.Threading.Timer(new TimerCallback(OnClock), null, 1000, 1000);
 
             while (true)
             {
@@ -44,8 +71,47 @@ namespace LCDili9341Test
 
                     tftLcd.DrawString(50, 50, line1, 0x0000FF, font);
                     tftLcd.DrawString(50, 60, line2, 0x0000FF, font);
+
+
+                    string keystr = "Key: " + teclado;
+                    //Font font = new HelpersFont(Verdana14.Bitmaps, Verdana14.Descriptors, Verdana14.Height, 5);
+                    tftLcd.DrawString(50, 100, keystr, 0x00ff00, font);
                 }
             }
+        }
+
+        private static void kb_OnKeyUp(uint KeyCode, uint Unused, DateTime time)
+        {
+            //string keystr = "Key pressed: " + KeyCode.ToString();
+            //Font font = new HelpersFont(Verdana14.Bitmaps, Verdana14.Descriptors, Verdana14.Height, 0);
+            //tftLcd.DrawString(50, 80, keystr, 0x00ff00, font);
+        }
+
+        private static void kb_OnKeyDown(uint KeyCode, uint Unused, DateTime time)
+        {
+            //tftLcd.ClearScreen();
+            //string keystr = "Tecla: " + keyMap[KeyCode];
+            //Font font = new HelpersFont(Verdana14.Bitmaps, Verdana14.Descriptors, Verdana14.Height, 5);
+            //tftLcd.DrawString(50, 100, keystr, 0x00ff00, font);
+
+            // * = ESC
+            if (KeyCode == 12)
+            {
+                teclado = string.Empty;
+                tftLcd.DrawString(50, 100, "                         ", 0x00ff00, font);
+                return;
+            }
+
+            // # = ENTER
+            if (KeyCode == 14)
+            {
+                return; 
+            }
+
+            teclado = teclado + keyMap[KeyCode];
+
+
+
         }
 
         private static void OnClock(object state)
